fb8d200bfc713b7536d24e62ddee3daffce1f259
luisflv <luis_flv@hotmail.com.br>
2015-06-18 13:10:12 -0300

    Criando projeto de testes e entidades

---
+++ ProjectMusicStore/MusicStore.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MusicStore.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Criando_Um_Instrumento_Musical()
+        {
+
+
+        }
+    }
+}
